bf7ca6bd73819f227f9d3825f6870de6a5f0bc09
2303A51688 <2303A51688@sru.edu.in>
2026-01-30 16:37:19 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+// import http module
+const http = require('http');
+
+// create server
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World');
+});
+
+// define port
+const PORT = 5000;
+
+// start server
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
